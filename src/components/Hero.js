9a6184270a4296b669cf429814aa8af5ae482d02
Gino <ginohagad@gmail.com>
2020-08-17 23:03:16 +1200

    Started using flexbox for the Hero section.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,8 +3,9 @@ import devices from '../images/devices.png'
 
 export default function Hero() {
   return (
-    <div className="py-20">
-      <img className="h-64 mx-auto" src={devices} alt="Devices" />
+    <div className="bg-gray-100 py-20 flex">
+      <img className="flex-auto h-64" src={devices} alt="Devices" />
+      <h3 className="flex-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis architecto, cum accusamus aperiam beatae porro in nobis cupiditate iste optio? Enim iusto error accusantium provident amet nesciunt dolorem, animi laboriosam!</h3>
     </div>
   )
 }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Intro() {
   return (
-		<div className="bg-gray-100">
+		<div>
 			<div className="container mx-auto py-10">
         <h1 className="text-3xl text-gray-700 text-center">Freelance Web Developer based in Wellington, New Zealand </h1>
 			</div>
