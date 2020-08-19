760f5663dfa6d3e726c4078acd6380382838ced5
Gino <ginohagad@gmail.com>
2020-08-19 16:11:42 +1200

    Made some minor changes but reverted back to the same code as last commit.

--- src/components/Main/Projects.js
+++ src/components/Main/Projects.js
@@ -4,16 +4,16 @@ export default function Projects() {
   return (
     <div className="py-20">
       <div className="flex flex-no-wrap px-20">
-        <img className="w-2/5 flex p-2" src="https://via.placeholder.com/300x200" alt="project"/>
-        <img className="w-2/5 flex p-2" src="https://via.placeholder.com/300x200" alt="project"/>
-        <img className="w-2/5 flex p-2" src="https://via.placeholder.com/300x200" alt="project"/>
-        <img className="w-2/5 flex p-2" src="https://via.placeholder.com/300x200" alt="project"/>
+        <img className="w-1/4 flex p-2" src="https://via.placeholder.com/300x200" alt="project"/>
+        <img className="w-1/4 flex p-2" src="https://via.placeholder.com/300x200" alt="project"/>
+        <img className="w-1/4 flex p-2" src="https://via.placeholder.com/300x200" alt="project"/>
+        <img className="w-1/4 flex p-2" src="https://via.placeholder.com/300x200" alt="project"/>
       </div>
       <div className="flex flex-no-wrap px-20">
-        <img className="w-2/5 flex p-2" src="https://via.placeholder.com/300x200" alt="project"/>
-        <img className="w-2/5 flex p-2" src="https://via.placeholder.com/300x200" alt="project"/>
-        <img className="w-2/5 flex p-2" src="https://via.placeholder.com/300x200" alt="project"/>
-        <img className="w-2/5 flex p-2" src="https://via.placeholder.com/300x200" alt="project"/>
+        <img className="w-1/4 flex p-2" src="https://via.placeholder.com/300x200" alt="project"/>
+        <img className="w-1/4 flex p-2" src="https://via.placeholder.com/300x200" alt="project"/>
+        <img className="w-1/4 flex p-2" src="https://via.placeholder.com/300x200" alt="project"/>
+        <img className="w-1/4 flex p-2" src="https://via.placeholder.com/300x200" alt="project"/>
       </div>
     </div>
   );
