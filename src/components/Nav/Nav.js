15f12af0682922098ebc02751df8f01aca6dc8f3
Gino <ginohagad@gmail.com>
2020-08-20 16:56:00 +1200

    Updated navbar to be responsive for mobile.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,16 +5,16 @@ const Nav = (props) => {
 
 
 	return (
-		<nav className="flex bg-green-700 py-6 px-20">
+		<nav className="flex bg-green-700 py-6 px-10">
 			<div className="container mx-auto flex items-center text-green-300">
 				<a href="#" className="font-semibold text-xl tracking-tight hover:text-white">GinoHagad</a>
 			</div>
-			<div className="block lg:hidden">
+			<div className="block md:hidden">
 				<button className="flex items-center px-3 py-2 border rounded text-green-200 border-green-400 hover:text-white hover:border-white">
 					<svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
 				</button>	
 			</div>
-			<div className="flex px-2 pt-2">
+			<div className="hidden md:flex px-2 pt-2">
 				<a href="#" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-green-800 hover:text-green-200">Home</a>
 				<a href="#" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-green-800 hover:text-green-200">About</a>
 				<a href="#" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-green-800 hover:text-green-200">Projects</a>
