ddabe42da3e3bcaa578c0063746d834a8c1821db
Gino <ginohagad@gmail.com>
2020-08-20 17:08:28 +1200

    Made Intro and Hero section responsive for mobile.

--- src/components/Main/Hero.js
+++ src/components/Main/Hero.js
@@ -3,9 +3,9 @@ import devices from '../../images/devices.png';
 
 export default function Hero() {
   return (
-    <div className="bg-gray-200 flex p-20">
-      <img className="flex-1 w-20 p-10" src={devices} alt="Devices" />
-      <h2 className="flex-1 text-xl p-10">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium quos expedita quasi similique assumenda minus.</h2>
+    <div className="bg-gray-200 p-10 md:flex md:p-20">
+      <img className="block md:flex-1 md:w-40 md:p-10" src={devices} alt="Devices" />
+      <h2 className="block pt-10 md:flex-1 md:text-xl md:p-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium quos expedita quasi similique assumenda minus.</h2>
     </div>
   )
 }
--- src/components/Main/Intro.js
+++ src/components/Main/Intro.js
@@ -4,7 +4,7 @@ export default function Intro() {
   return (
 		<div>
 			<div className="container mx-auto py-10">
-        <h1 className="text-3xl text-gray-700 text-center">Freelance Web Developer based in Wellington, New Zealand </h1>
+        <h1 className="text-3xl text-gray-700 text-center px-10 md:px-20">Freelance Web Developer based in Wellington, New Zealand </h1>
 			</div>
 		</div>
   )
