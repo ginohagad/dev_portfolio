f38626c77ee97eae98b08377a8f6e5bc19257f04
Gino <ginohagad@gmail.com>
2022-04-18 23:54:27 +1200

    Assigned nav links.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,7 +5,7 @@ const Nav = (props) => {
 	return (
 		<nav className="flex bg-green-700 py-6 px-10">
 			<div className="container mx-auto flex items-center text-green-300">
-				<a href="#" className="font-semibold text-xl tracking-tight hover:text-white">GinoHagad</a>
+				<a href="#home" className="font-semibold text-xl tracking-tight hover:text-white">GinoHagad</a>
 			</div>
 			<div className="block md:hidden">
 				<button className="flex items-center px-3 py-2 border rounded text-green-200 border-green-400 hover:text-white hover:border-white">
@@ -13,9 +13,9 @@ const Nav = (props) => {
 				</button>	
 			</div>
 			<div className="hidden md:flex px-2 pt-2">
-				<a href="#" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-green-800 hover:text-green-200">Home</a>
-				<a href="#" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-green-800 hover:text-green-200">About</a>
-				<a href="#" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-green-800 hover:text-green-200">Projects</a>
+				<a href="#home" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-green-800 hover:text-green-200">Home</a>
+				<a href="#about" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-green-800 hover:text-green-200">About</a>
+				<a href="#projects" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-green-800 hover:text-green-200">Projects</a>
 			</div>
 		</nav>
 	);
